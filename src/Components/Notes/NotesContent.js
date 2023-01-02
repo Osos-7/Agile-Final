30ecd6c46c3f0f6f2b3707d21d0d34fddd069043
Osos-7 <osos.amr2002@gmail.com>
2023-01-02 21:16:28 +0200

    videos updated

--- src/Components/Notes/NotesContent.js
+++ src/Components/Notes/NotesContent.js
@@ -6,7 +6,7 @@ const VideosContent = (props) => {
   const { imgUrl, title, path } = props.item;
 
   return (
-    <div className="single_item">
+    <div className="notes_item">
       <div className="notes_card">
       <Link to={path} style={{ textDecoration: 'none', color:"black" }}>
         <div className="notes_section">
